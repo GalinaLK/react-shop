b4b8df1d01fd317fa6b50b9235db5fc414e2f900
GalinaLK <gangnam0803@gmail.com>
2022-01-12 12:02:49 +0200

    banner bg

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,11 +6,13 @@ const Cards = (props) => {
 
     return (
         <section className="card-item">  
+         <div className='photo-card'>
          <picture>
            <source media="(max-width: 499px)" srcSet={model24}/>
            <source media="(min-width: 500px)" srcSet={model24}/>
            <img src={model24}  alt={alt}/>
          </picture>
+         </div>       
          <p>{alt}</p>
          <p className="price-card">$49.99</p>
         </section>
