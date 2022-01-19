eea345de458d01a2077dd0fcab0e6c39fea2807c
GalinaLK <gangnam0803@gmail.com>
2022-01-19 11:48:04 +0200

    nav hidden

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
     }
   }
     return (
-      <header className="header-main container">
+      <header className="header-main">
         <nav className="nav-desc">
           <div className="top-nav">
             <Link to="/" id='logo'>
